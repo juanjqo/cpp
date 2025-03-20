dffd1450212824556954ba750af4d315db17ffcf
Juancho <juanjqogm@gmail.com>
2025-03-20 11:31:33 +0000

    Added the class DQ_Kinetics

---
+++ include/dqrobotics/robot_modeling/DQ_Kinetics.h
@@ -0,0 +1,54 @@
+/**
+(C) Copyright 2011-2025 DQ Robotics Developers
+
+This file is part of DQ Robotics.
+
+    DQ Robotics is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DQ Robotics is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+- Juan Jose Quiroz Omana (juanjose.quirozomana@manchester.ac.uk)
+*/
+
+#pragma once
+
+#include<dqrobotics/DQ.h>
+#include<memory>
+
+namespace DQ_robotics
+{
+class DQ_Kinetics
+{
+protected:
+
+    void _check_inertial_parameters();
+    std::vector<Matrix<double, 3,3>> inertia_tensors_;
+    std::vector<Vector3d> center_of_masses_;
+    std::vector<double> masses_;
+    int number_of_bodies_;
+    DQ_Kinetics();
+
+public:
+    virtual ~DQ_Kinetics() = default;
+    std::vector<Matrix<double, 3,3>>  get_inertia_tensors() const;
+    std::vector<Vector3d>             get_center_of_masses() const;
+    std::vector<double>               get_masses() const;
+    int                               get_number_of_bodies() const;
+
+    void set_inertia_tensors(const std::vector<Matrix<double,3,3>>& inertia_tensors);
+    void set_center_of_masses(const std::vector<Vector3d>& center_of_masses);
+    void set_masses(const std::vector<double>& masses);
+
+
+};
+}
---
+++ src/robot_modeling/DQ_Kinetics.cpp
@@ -0,0 +1,88 @@
+/**
+(C) Copyright 2011-2025 DQ Robotics Developers
+
+This file is part of DQ Robotics.
+
+    DQ Robotics is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DQ Robotics is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+- Juan Jose Quiroz Omana (juanjose.quirozomana@manchester.ac.uk)
+*/
+
+#include <dqrobotics/robot_modeling/DQ_Kinetics.h>
+namespace DQ_robotics
+{
+DQ_Kinetics::DQ_Kinetics(){
+
+}
+
+std::vector<Matrix<double, 3, 3> > DQ_Kinetics::get_inertia_tensors() const
+{
+    return inertia_tensors_;
+}
+
+std::vector<Vector3d> DQ_Kinetics::get_center_of_masses() const
+{
+    return center_of_masses_;
+}
+
+std::vector<double> DQ_Kinetics::get_masses() const
+{
+    return masses_;
+}
+
+int DQ_Kinetics::get_number_of_bodies() const
+{
+    return number_of_bodies_;
+}
+
+void DQ_Kinetics::set_inertia_tensors(const std::vector<Matrix<double, 3, 3> > &inertia_tensors)
+{
+    inertia_tensors_ = inertia_tensors;
+}
+
+void DQ_Kinetics::set_center_of_masses(const std::vector<Vector3d> &center_of_masses)
+{
+    center_of_masses_ = center_of_masses;
+}
+
+void DQ_Kinetics::set_masses(const std::vector<double> &masses)
+{
+    masses_ = masses;
+}
+
+void DQ_Kinetics::_check_inertial_parameters()
+{
+    std::size_t s_masses = masses_.size();
+    std::size_t s_coms   = center_of_masses_.size();
+    std::size_t s_inertias = inertia_tensors_.size();
+    if ((s_masses == s_coms) && (s_coms == s_inertias))
+    {
+        if(s_coms == 0)
+        {
+            throw std::runtime_error(std::string("Undefined inertial parameters!"));
+        }else{
+            number_of_bodies_ = s_masses;
+        }
+
+    }else
+    {
+        throw std::runtime_error(std::string("The inertial parameters have incompatible sizes!"));
+    }
+
+
+}
+
+}
+
