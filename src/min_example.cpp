f14f69df65b9978e0b32e8f7d138c76f6cfcefef
Juancho <juanjqogm@gmail.com>
2025-04-23 09:01:20 +0100

    Updated the min_example

--- src/min_example.cpp
+++ src/min_example.cpp
@@ -11,23 +11,19 @@ int main(void)
 {
     auto robot = std::make_shared<DQ_SerialManipulatorMDH>
         (FrankaEmikaPandaRobot::dynamics());
-    //robot->set_gravity_acceleration(DQ{0});
 
     const auto q      = (VectorXd(7) << 1,2,3,4,0,0,0).finished();
     const auto q_dot  = (VectorXd(7) << 0,0,0,5,6,7,8).finished();
     const auto q_ddot = (VectorXd(7) << -1,0,-4,0,0,0,0).finished();
 
-    //Gauss Principle of Least Constraint solver
-
     robot->dynamic_solver(std::make_shared<DQ_GaussPrincipleSolver>());
-    auto forces = robot->compute_generalized_forces(q, q_dot, q_ddot);
-    std::cout<<forces.transpose()<<std::endl;
 
+    auto forces = robot->compute_generalized_forces(q, q_dot, q_ddot);
     auto M = robot->compute_inertia_matrix(q);
     auto c = robot->compute_coriolis_vector(q, q_dot);
     auto g = robot->compute_gravitational_forces_vector(q);
 
-
+    std::cout<<forces.transpose()<<std::endl;
     auto robot2 = std::make_shared<DQ_SerialManipulatorMDH>
         (FrankaEmikaPandaRobot::dynamics());
     //robot2->set_gravity_acceleration(DQ{0});
